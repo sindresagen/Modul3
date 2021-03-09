fe39c2a5fc2b733aba5bd325cc078aa00f84a252
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-03-09 20:22:43 +0100

    intro oo programming

---
+++ ConsoleApp1/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Box
+    {
+        public int Width;
+        public int Height;
+
+        internal void Show()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("Bredden er " + Width + " og høyden er " + Height);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // JS:
+            // var x = { width: 50, height: 50 };
+            // C#:
+            //var x = new { Width = 50, Height = 30 };
+            //var x = new Box();
+            //x.Width = 50;
+            //x.Height = 50;
+            // Dette er sånn det skal gjøres i C#, det over er bare unntaksvis fornuftig
+            var x = new Box { Width = 50, Height = 50 };
+            x.Show();
+        }
+    }
+}
