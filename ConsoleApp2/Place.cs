4c3538e3687c2c3fec8e5ba63b8cd5217a8e5c99
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-03-10 11:21:45 +0100

    intro oo prog

---
+++ ConsoleApp2/Place.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Place
+    {
+        public static string PlaceName;
+        public static string Municipality;
+        public static string Region;
+
+        public static void ShowPlace()
+        {
+            var labelWidth = 8;
+            ShowSepRow(8);
+            ShowFieldNameAndValue("Navn", labelWidth, PlaceName);
+            ShowFieldNameAndValue("Kommune", labelWidth, Municipality);
+            ShowFieldNameAndValue("Fylke", labelWidth, Region);
+            ShowSepRow(labelWidth);
+        }
+
+
+        private static void ShowFieldNameAndValue(string label, int labelWidth, string fieldValue)
+        {
+            labelWidth -= label.Length;
+            Console.WriteLine("  " + label + ":" + string.Empty.PadLeft(labelWidth, ' ') + fieldValue);
+        }
+
+        //static void ShowSepRow()
+        //{
+        //    ShowSepRow(8);
+        //}
+        static void ShowSepRow(int labelWidth)
+        {
+            labelWidth += 14;
+            Console.WriteLine(string.Empty.PadLeft(labelWidth, '*'));
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var labelWidth = 8;
+            //ShowSepRow(8);
+            //var place = "Stavern";
+            //ShowFieldNameAndValue("Navn", labelWidth, place);
+            //var municipality = "Larvik";
+            //ShowFieldNameAndValue("Kommune", labelWidth, municipality);
+            //var region = "Vestfold";
+            //ShowFieldNameAndValue("Fylke", labelWidth, region);
+            //ShowSepRow(labelWidth);
+            //var place = "Stavern";
+            //var municipality = "Larvik";
+            //var region = "Vestfold";
+            //ShowPlace(place, municipality, region);
+            Place.PlaceName = "Stavern";
+            Place.Municipality = "Larvik";
+            Place.Region = "Vestfold";
+            Place.ShowPlace();
+
+            Place.PlaceName = "Rjukan";
+            Place.Municipality = "Tinn";
+            Place.Region = "Telemark";
+            Place.ShowPlace();
+        }
+
+    }
+}
