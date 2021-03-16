0177e7700e4174d9b79a53a4ca7ce189d4e7b23d
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-03-16 14:37:33 +0100

    *CLAP* CODE *CLAP* REVIEW!

---
+++ CodeReviewRee/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeReviewRee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = { "Erik", "Henrik", "Kristian", "Stian", "Mikael", "Angelica", "Sondre", "Theodor", "Sigve" };
+
+            Students.VælkømminTekst(students);
+            var userInput = Console.ReadLine();
+            if (userInput == "Y" || userInput == "y") Students.studentPlukkern(students);
+            else Console.WriteLine("Neivel!");
+        }
+    }
+}
---
+++ CodeReviewRee/Students.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeReviewRee
+{
+    class Students
+    {
+        public static void studentPlukkern(string[] students)
+        { 
+        Random r = new Random();
+
+        int index1 = r.Next(students.Length);
+        int index2 = r.Next(students.Length);
+
+        Console.WriteLine($"De neste studentene til å bli fugged er {students[index1]} og {students[index2]}!!1!");
+    }
+
+    public static void VælkømminTekst(string[] students)
+    {
+    Console.Write("Hei!\r\nVil du velge to stakkarer som skal vise frem neste uke?\r\n(Y / N)\r\n");
+    }
+}
+}
